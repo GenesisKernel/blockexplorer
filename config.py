31e3effb5a00e9192ed3c2300a9fbcecd25dc384
blitzstern5 <blitzstern5@gmail.com>
2018-07-02 04:23:18 +0200

    Add backend 20180512 version to version/features map

--- config.py
+++ config.py
@@ -16,6 +16,16 @@ DB_ENGINE_DISCOVERY_MAP = {
 }
 
 BACKEND_VERSION_FEATURES_MAP = {
+    20180512: {
+        'github-branch': 'develop',
+        'github-commmit': '4b69b8e',
+        'url': 'https://github.com/GenesisKernel/go-genesis/pull/290',
+        'features': [
+            'system_parameters_at_ecosystem',
+            'image_id_instead_of_avatar',
+            'member_info_at_members',
+        ]
+    }
 }
 
 POSTS_PER_PAGE = 3
