c024e257dc254c868d3e469a70ca60fa177691ba
blitzstern5 <blitzstern5@gmail.com>
2018-11-20 15:13:03 +0300

    Change run-simple port to 8080

--- run-simple.py
+++ run-simple.py
@@ -5,4 +5,4 @@ app = create_app(debug=True)
 app.app_context().push()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug = True)
+    app.run(host='0.0.0.0', port=8080, debug = True)
