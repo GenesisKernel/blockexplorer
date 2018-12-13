ec5edc7378f27b5fc9aed7dc2639d743540be2e4
blitzstern5 <blitzstern5@gmail.com>
2018-12-12 21:37:25 -0500

    Remove old runner files

--- run-gunicorn.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-from genesis_block_explorer.app import create_app
-app = create_app(debug=True)
-#app.app_context().push()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug = True)
--- run-gunicorn2.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env python
-
-from genesis_block_explorer.socketio import socketio, app
