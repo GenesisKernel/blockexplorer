357c6584f461d4ec06ef9de480fd13a6fb4c8d01
blitzstern5 <blitzstern5@gmail.com>
2018-12-12 21:44:00 -0500

    Remove old debug runner

--- run-debug.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-
-from genesis_block_explorer.socketio import socketio, app
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
-    #app.run(host='0.0.0.0', debug = True)
